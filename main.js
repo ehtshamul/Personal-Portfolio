0a5af8ff94fcf96a4b07806001de57bff369cf75
ehtsham ul haq <128890292+ehtshamul@users.noreply.github.com>
2025-08-02 11:57:57 +0500

    Update main.js
    Signed-off-by: ehtsham ul haq  <128890292+ehtshamul@users.noreply.github.com>

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ app.use((err, req, res, next) => {
 // MongoDB and server start
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/haq_portfolio", {
+    await mongoose.connect(process.env.MONGO_URI  {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
